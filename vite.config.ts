39e564965b6eccc4e6d20383fd54c43092570538
Orion1ddev <orion1d.dev@gmail.com>
2025-01-10 22:36:32 +0300

    aaa

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist', // Ensures the build output matches the Wrangler config
+    emptyOutDir: true,
   },
-  base: "./",
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
+});
